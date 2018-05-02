43295c4de4937245452fc1ddb2bf9fcbaf3d726d
lzhadid <lina.zhadid@gmail.com>
2018-05-02 09:42:48 +0200

    Add files via upload

--- Task3_IonizationRate/functions.py
+++ Task3_IonizationRate/functions.py
@@ -1,8 +1,14 @@
 import numpy as np
 
-__all__ = ['find_nearest']
+__all__ = ['find_nearest','fit_exp_linear']
 
 
 def find_nearest(array,value):
     idx = (np.abs(array-value)).argmin()
     return array[idx]
+    
+def fit_exp_linear(x, y):
+    y_log = np.log(y)
+    K, A_log = np.polyfit(x, y_log,1)
+    A = np.exp(A_log)
+    return A, K
